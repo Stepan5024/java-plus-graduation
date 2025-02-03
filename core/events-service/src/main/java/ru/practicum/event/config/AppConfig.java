c965cb80bb4581fdd6b5bd0dab5a2aecbe322956
Степан Бокарев <bokarevstepan1@yandex.ru>
2025-02-03 19:01:39 +0300

    изменил компосе

--- core/events-service/src/main/java/ru/practicum/event/config/AppConfig.java
+++ core/events-service/src/main/java/ru/practicum/event/config/AppConfig.java
@@ -1,7 +1,9 @@
 package ru.practicum.event.config;
 
+import feign.Feign;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -9,4 +11,10 @@ import org.springframework.context.annotation.Configuration;
 public class AppConfig {
     @Value("${service.name}")
     private String appName;
+
+    @Bean
+    public Feign.Builder feignBuilder() {
+
+        return Feign.builder();
+    }
 }
